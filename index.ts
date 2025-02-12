cd2dd609344e121a5e04df663efef766224e78ba
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-02-11 22:01:11 -0500

    add server

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client'
+
+import { config } from 'dotenv'
+config()
+
+const prisma = new PrismaClient()
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('test')
+})
+
+export default {
+  port: 3000,
+  fetch: app.fetch
+}
